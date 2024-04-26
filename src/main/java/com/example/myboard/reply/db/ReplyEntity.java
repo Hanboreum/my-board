6c1f0169d72d30dd694a3e03bf0b42436b02232b
fullm <matoma1_@naver.com>
2024-04-27 01:20:40 +0900

    password 타입 변경

--- src/main/java/com/example/myboard/reply/db/ReplyEntity.java
+++ src/main/java/com/example/myboard/reply/db/ReplyEntity.java
@@ -19,7 +19,8 @@ public class ReplyEntity {
     private Long id;
     private Long postId;
     private String userName;
-    private  int password;
+    private  String password;
+    private String status;
     private String title;
     @Column(columnDefinition = "TEXT")
     private String content;
