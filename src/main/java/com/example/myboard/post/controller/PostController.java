5aca572e93ff455795ef6aa81d1cbfd26a4da46d
fullm <matoma1_@naver.com>
2024-04-27 00:32:15 +0900

    개별 조회, 전체 조회, 삭제 (상태 변경)

--- src/main/java/com/example/myboard/post/controller/PostController.java
+++ src/main/java/com/example/myboard/post/controller/PostController.java
@@ -2,13 +2,13 @@ package com.example.myboard.post.controller;
 
 import com.example.myboard.post.db.PostEntity;
 import com.example.myboard.post.model.PostRequest;
+import com.example.myboard.post.model.PostViewRequest;
 import com.example.myboard.post.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +19,22 @@ public class PostController {
     @PostMapping("")
     public PostEntity create(@Valid @RequestBody PostRequest postRequest){
         return postService.create(postRequest);
+    }
+
+    //@GetMapping("/id/{id}")
+    @PostMapping("/view") //개별 조회
+    public PostEntity view(@Valid @RequestBody PostViewRequest postViewRequest){
+        return postService.view(postViewRequest);
+    }
+    @GetMapping("/all")
+    public List<PostEntity> list(){ //List를 뿌려야 하기 떄문에 만듬
+        return postService.all();
+    }
+    @PostMapping("/delete")
+    public void delete(
+            @Valid
+            @RequestBody PostViewRequest postViewRequest
+    ){
+        postService.delete(postViewRequest);
     }
 }
--- src/main/java/com/example/myboard/post/service/PostService.java
+++ src/main/java/com/example/myboard/post/service/PostService.java
@@ -3,6 +3,7 @@ package com.example.myboard.post.service;
 import com.example.myboard.post.db.PostEntity;
 import com.example.myboard.post.db.PostRepository;
 import com.example.myboard.post.model.PostRequest;
+import com.example.myboard.post.model.PostViewRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -28,4 +29,49 @@ public class PostService {
         return postRepository.save(entity);
     }
 
+    /**
+     * view는 두가지 검증
+     * 게시글 유무
+     * 비밀번호
+     */
+    public PostEntity view(PostViewRequest postViewRequest) {
+       return postRepository.findById(postViewRequest.getPostId())
+               .map( it->{
+                   //entity가 존재 할때만
+                   if(! it.getPassword().equals(postViewRequest.getPassword())){
+                       //db에 있는 post의 pw와 입력 받은 post의 pw를 비교해 동일하지 않다면
+                       var format = "비밀번호가 맞지 않습니다. %s vs %s ";
+                       throw new RuntimeException(String.format(format,it.getPassword(), postViewRequest.getPassword()));
+                   }
+                   return  it; //맞는다면 해당 entity  리턴
+
+               }).orElseThrow( //데이터가 없다면
+                       ()->{
+                           return new RuntimeException("해당 게시글이 존재하지 않습니다."+ postViewRequest.getPostId());
+                       }
+               );
+    }
+
+    public List<PostEntity> all() {
+        return postRepository.findAll();
+    }
+
+    public void delete(PostViewRequest postViewRequest) {
+        postRepository.findById(postViewRequest.getPostId())
+                .map( it -> {
+                    // entity 존재
+                    if(!it.getPassword().equals(postViewRequest.getPassword())){
+                        var format = "패스워드가 맞지 않습니다 %s vs %s";
+                        throw new RuntimeException(String.format(format, it.getPassword(), postViewRequest.getPassword()));
+                    }
+
+                    it.setStatus("UNREGISTERED");
+                    postRepository.save(it);
+                    return it;
+                }).orElseThrow(
+                        ()-> {
+                            return new RuntimeException("해당 게시글이 존재 하지 않습니다 : "+postViewRequest.getPostId());
+                        }
+                );
+    }
 }
